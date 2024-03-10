8d08062cf709ad29a77004c95035b39893439b4d
slgeay <avomailbox-github@yahoo.fr>
2024-03-10 12:17:35 +0100

    Refactor dont_panic.py to optimize dictionary initialization

--- src/codegolf/easy/dont_panic.py
+++ src/codegolf/easy/dont_panic.py
@@ -1,6 +1,5 @@
 I,L=int,input
 M=lambda _:map(I,L().split())
 _,W,_,F,X,_,_,E=M(0)
-e=dict(map(M,[0]*E))
-while 1:f,p,r=L().split();f,p,r=I(f),I(p),r>"R";x=[e.get(f,[0,W-1][r]),X][f==F];print("BWLAOICTK"[(f<0)|(p==x)|r^(p>x)::2])
-
+e={F:X,**dict(map(M,[0]*E))}
+while 1:f,p,r=L().split();f,p,r=I(f),I(p),r>"R";print("BWLAOICTK"[f<0 or p==e[f]or(p>e[f])^r::2])
