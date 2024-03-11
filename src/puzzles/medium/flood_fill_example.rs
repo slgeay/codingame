7effd87cc0957be9d3c4d50cdbfe9f0aec01132f
slgeay <avomailbox-github@yahoo.fr>
2024-03-11 22:00:22 +0100

    Refactor grid and troops logic

--- src/puzzles/medium/flood_fill_example.rs
+++ src/puzzles/medium/flood_fill_example.rs
@@ -1,44 +1,113 @@
-use std::io;
+use std::{io,collections::{HashMap, hash_map::Entry::{Vacant, Occupied}}};
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
 
-macro_rules! eprint_grid {
-    ($h:expr, $grid:expr) => {
-        for i in 0..$h as usize {
-            eprintln!("{}", $grid[i]);
-        }
-        eprintln!("");
-    };
+// Record towers ID and use Unique IDs instead
+struct TowersRegistry {
+    uid_to_id: HashMap::<char, char>,
+    next_uid: char
+}
+
+impl TowersRegistry {
+    fn new() -> TowersRegistry {
+        TowersRegistry { uid_to_id: HashMap::<char, char>::new(), next_uid: 'A' }
+    }
+
+    fn add(&mut self, id:char) -> char {
+        let uid = self.next_uid;
+        self.uid_to_id.insert(uid, id);
+        self.next_uid = (uid as u8 + 1) as char;
+        uid
+    }
+
+    fn get(&self, uid:&char) -> char {
+        self.uid_to_id[uid]
+    }
+}
+
+// Handle grid logic
+struct Grid {
+    grid: Vec::<String>
+}
+
+impl Grid {
+    fn new() -> Grid {
+        Grid { grid: Vec::<String>::new() }
+    }
+
+    fn push(&mut self, line: String) {
+        self.grid.push(line)
+    }
+
+    fn get(&self, x: usize, y: usize) -> char {
+        self.grid[y].chars().nth(x).unwrap()
+    }
+
+    fn replace(&mut self, x: usize, y: usize, c: char) {
+        self.grid[y].replace_range(x..x+1, c.to_string().as_str());
+    }
+
+    fn get_line(&self, y: usize) -> &String {
+        &self.grid[y]
+    }
+}
+
+// Handle troops logic, notably simultaneous arrivals
+struct Troops {
+    troops: HashMap::<(usize, usize), char>
+}
+
+impl Troops {
+    fn new() -> Troops {
+        Troops { troops:HashMap::<(usize, usize), char>::new() }
+    }
+
+    fn push(&mut self, x: usize, y: usize, uid: char) {
+        match self.troops.entry((x,y)) {
+            Vacant(v) => {
+                v.insert(uid);
+            },
+            Occupied(mut o) => {
+                if *o.get() != uid {
+                    o.insert('+');
+                }
+            }
+        };
+    }
+
+    fn is_empty(&self) -> bool {
+        self.troops.is_empty()
+    }
 }
 
 fn main() {
+    // Read inputs
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let w = parse_input!(input_line, i32);
+    let w = parse_input!(input_line, usize);
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let h = parse_input!(input_line, i32);
+    let h = parse_input!(input_line, usize);
 
-    let mut grid = Vec::<String>::new();
-    let mut troops = Vec::<(i32, i32, char)>::new();
+    let mut grid = Grid::new();
+    let mut towers = TowersRegistry::new();
+    let mut current_troops = Troops::new();
 
-    let mut towers = std::collections::HashMap::<char, char>::new();
-    let mut next_tower = 'A';
-
-
-    for i in 0..h as usize {
+    // Read the input grid
+    for y in 0..h as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let line = input_line.trim_matches('\n').to_string();
         let mut new_line = String::new();
-        for (j, c) in line.chars().enumerate() {
+
+        for (x, c) in line.chars().enumerate() {
             if c != '.' && c != '#' {
-                towers.insert(next_tower, c);
-                new_line.push(next_tower);
-                troops.push((j as i32, i as i32, next_tower));
-                next_tower = (next_tower as u8 + 1) as char;
+                // Register the tower and its current troops
+                let uid = towers.add(c);
+                new_line.push(uid);
+                current_troops.push(x, y, uid);
             } else {
                 new_line.push(c);
             }
@@ -46,46 +115,42 @@ fn main() {
         grid.push(new_line);
     }
 
-    while !troops.is_empty() {
-        eprint_grid!(h, &grid);
-        let mut new_troops = Vec::<(i32, i32, char)>::new();
-        for (x, y, t) in troops {
-            if x > 0 && grid[y as usize].chars().nth(x as usize - 1).unwrap() == '.' {
-                new_troops.push((x - 1, y, t));
+    // Deploy troops by waves    
+    while !current_troops.is_empty() {
+        let mut next_troops = Troops::new();
+
+        // Compute next troops (Manhattan geometry)
+        for ((x, y), t) in current_troops.troops {
+            if x > 0 && grid.get(x - 1, y) == '.' {
+                next_troops.push(x - 1, y, t);
             }
-            if x < w - 1 && grid[y as usize].chars().nth(x as usize + 1).unwrap() == '.' {
-                new_troops.push((x + 1, y, t));
+            if x < w - 1 && grid.get(x + 1, y) == '.' {
+                next_troops.push(x + 1, y, t);
             }
-            if y > 0 && grid[y as usize - 1].chars().nth(x as usize).unwrap() == '.' {
-                new_troops.push((x, y - 1, t));
+            if y > 0 && grid.get(x, y - 1) == '.' {
+                next_troops.push(x, y - 1, t);
             }
-            if y < h - 1 && grid[y as usize + 1].chars().nth(x as usize).unwrap() == '.' {
-                new_troops.push((x, y + 1, t));
+            if y < h - 1 && grid.get(x, y + 1)== '.' {
+                next_troops.push(x, y + 1, t);
             }
         }
-
-        new_troops.sort();
-        new_troops.dedup();
         
-        for (x, y, t) in new_troops.iter() {
-            if new_troops.iter().filter(|(x2, y2, _)| x == x2 && y == y2).count() > 1 {
-                grid[*y as usize].replace_range(*x as usize..*x as usize + 1, "+");
-            } else {
-                grid[*y as usize].replace_range(*x as usize..*x as usize + 1, t.to_string().as_str());
-            }
+        // Deploy troops
+        for ((x, y), uid) in next_troops.troops.iter() {
+            grid.replace(*x, *y, *uid);
         }
  
-        troops = new_troops;
+        // Prepare next wave
+        current_troops = next_troops;
     }
-    eprint_grid!(h, &grid);
-
-    for (k, v) in towers.iter() {
-        eprintln!("{} {}", k, v);
-    }
-    eprintln!("");
     
-    for i in 0..h as usize {
-        grid[i] = grid[i].chars().map(|c: char| if c == '.' || c == '#' || c == '+' { c } else { towers[&c] }).collect();
-        println!("{}", grid[i]);
+    // Replace UIDs with actual IDs and print
+    for y in 0..h as usize {
+        let line: String = grid
+            .get_line(y)
+            .chars()
+            .map(|c: char| if c == '.' || c == '#' || c == '+' { c } else { towers.get(&c) }).
+            collect();
+        println!("{}", line);
     }
 }
