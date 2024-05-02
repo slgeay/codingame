4acb9fb71ac1834ca542843b0a10d888fb200780
slgeay <avomailbox-github@yahoo.fr>
2024-05-02 09:42:50 +0200

    Refactor paper_labyrinth.rs for improved pathfinding algorithms

--- src/puzzles/medium/paper_labyrinth.rs
+++ src/puzzles/medium/paper_labyrinth.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{cmp::Reverse, collections::BinaryHeap, io};
 
 #[derive(Clone, Copy)]
 enum Direction {
@@ -23,7 +23,7 @@ impl Direction {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord)]
 struct Vec2 {
     x: usize,
     y: usize,
@@ -52,6 +52,12 @@ impl Vec2 {
             Direction::Left => Vec2::new(self.x - 1, self.y),
         }
     }
+
+    fn distance(&self, to: Vec2) -> usize {
+        let dx = (self.x as i32 - to.x as i32).unsigned_abs() as usize;
+        let dy = (self.y as i32 - to.y as i32).unsigned_abs() as usize;
+        dx + dy
+    }
 }
 
 struct Cell {
@@ -79,6 +85,8 @@ impl Cell {
     }
 }
 
+// Display the cell as a character
+// We use lines to represent the paths we can take (without walls)
 impl std::fmt::Display for Cell {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let c = match self.walls {
@@ -140,17 +148,26 @@ impl Labyrinth {
     }
 
     // Breadth-first search
-    fn pathfinding(&self, from: Vec2, to: Vec2) -> Vec<Vec2> {
+    // We use a queue to visit all the neighbors of a cell before moving to the next one
+    // This way, we ensure to find the shortest path
+    // We also keep track of the previous cell of each visited cell to reconstruct the path
+    // This algorithm is not optimal, but it's easy to implement and works well for small labyrinth
+    fn breadth_first_search(&self, from: Vec2, to: Vec2) -> Vec<Vec2> {
         let mut path = Vec::<Vec2>::new();
 
-        let mut visited = vec![vec![false; self.size.x]; self.size.y];
         let mut queue = Vec::<Vec2>::new();
+        queue.push(from);
+
+        // Normally we track visited cells, but we can save space by using the previous cell to know if we visited it
         let mut prev = vec![vec![None; self.size.x]; self.size.y];
 
-        queue.push(from);
-        visited[from.y][from.x] = true;
+        // We mark the starting cell as visited by setting its previous cell to itself
+        prev[from.y][from.x] = Some(from);
+
+        let mut steps = 0;
 
         while !queue.is_empty() {
+            steps += 1;
             let current = queue.remove(0);
 
             // We found the path, let's reconstruct it
@@ -168,23 +185,84 @@ impl Labyrinth {
             let cell = self.get_cell(current);
             Direction::iter().for_each(|dir| {
                 if let Some(neighbor) = cell.neighbor(dir) {
-                    if visited[neighbor.y][neighbor.x] {
+                    if prev[neighbor.y][neighbor.x].is_some() {
                         return;
                     }
                     queue.push(neighbor);
-                    visited[neighbor.y][neighbor.x] = true;
                     prev[neighbor.y][neighbor.x] = Some(current);
                 }
             });
         }
 
+        eprintln!("Breadth-First Search : {} steps", steps);
+
+        path
+    }
+
+    // Best-first search
+    // We use a priority queue to visit the cell closest to the target first
+    // This algorithm is more efficient than the breadth-first search, as it explores the most promising path first
+    fn best_first_search(&self, from: Vec2, to: Vec2) -> Vec<Vec2> {
+        let mut path = Vec::<Vec2>::new();
+
+        let mut priority_queue = BinaryHeap::new();
+
+        // BinaryHeap is a max-heap, so we need to reverse the order to have a min-heap
+        // We use the distance already walked plus the shortest distance to the target possible as the priority
+        // We also keep that walked distance to calculate future priorities
+        priority_queue.push((Reverse(0 + from.distance(to)), 0, from));
+
+        let mut prev = vec![vec![None; self.size.x]; self.size.y];
+        // This time we need to track the distance walked to the cell
+        prev[from.y][from.x] = Some(from);
+
+        let mut steps = 0;
+
+        while let Some((_priority, walked_dist, current)) = priority_queue.pop() {
+            steps += 1;
+
+            // We found the path, let's reconstruct it
+            if current == to {
+                let mut current = to;
+                while current != from {
+                    path.push(current);
+                    current = prev[current.y][current.x].unwrap();
+                }
+                path.reverse();
+                break;
+            }
+
+            // Else, visit neighbors not visited yet, and update the queue
+            let cell = self.get_cell(current);
+            let walked_dist = walked_dist + 1;
+            Direction::iter().for_each(|dir| {
+                if let Some(neighbor) = cell.neighbor(dir) {
+                    if prev[neighbor.y][neighbor.x].is_some() {
+                        return;
+                    }
+                    priority_queue.push((
+                        Reverse(walked_dist + current.distance(to)),
+                        walked_dist,
+                        neighbor,
+                    ));
+                    prev[neighbor.y][neighbor.x] = Some(current);
+                }
+            });
+        }
+
+        eprintln!("Best-First Search : {} steps", steps);
+
         path
     }
 
     fn distance(&self, from: Vec2, to: Vec2) -> usize {
-        let path = self.pathfinding(from, to);
-        eprintln!("{:?}", path);
-        path.len()
+        let breadth_path = self.breadth_first_search(from, to);
+        let best_path = self.best_first_search(from, to);
+
+        // The two paths might not be the same, but they should have the same length
+        assert_eq!(breadth_path.len(), best_path.len());
+
+        best_path.len()
     }
 }
 
@@ -206,7 +284,7 @@ fn main() {
     let rabbit = Vec2::new_from_stdin();
     let labyrinth = Labyrinth::new_from_stdin();
 
-    eprintln!("{:?} {:?}", start, rabbit);
+    eprintln!("Start: {:?} ; Rabbit: {:?}", start, rabbit);
     eprintln!("{}", labyrinth);
 
     println!(
